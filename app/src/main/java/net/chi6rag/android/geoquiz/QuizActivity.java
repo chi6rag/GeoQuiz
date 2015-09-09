3634a2ce53c8e32adf394fb9982b0cf709adc4c8
Chirag Aggarwal <chi6rag@gmail.com>
2015-09-09 17:26:21 +0530

    - Add questions as string resources in string.xml - Add Question model, define setter and getters - Add references to questions in QuizActivity.java

--- app/src/main/java/net/chi6rag/android/geoquiz/QuizActivity.java
+++ app/src/main/java/net/chi6rag/android/geoquiz/QuizActivity.java
@@ -6,20 +6,40 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView mQuestionTextView;
+    private Button mNextButton;
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_oceans, true)
+
+    };
+    private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        // Inflate XML Widgets
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question);
+        mNextButton = (Button) findViewById(R.id.next_button);
 
+        // Set the text of mQuestionTextView to CurrentIndex of QuestionBank array
+        int mQuestionResId = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(mQuestionResId);
+
+        // Add event listeners to widgets
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +54,12 @@ public class QuizActivity extends AppCompatActivity {
                                 R.string.incorrect_toast,
                                 Toast.LENGTH_LONG
                 ).show();
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
             }
         });
     }
