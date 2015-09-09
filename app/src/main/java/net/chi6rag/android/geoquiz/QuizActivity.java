c37ca47b1e84b7933c72d061cffd3838ee504add
Chirag Aggarwal <chi6rag@gmail.com>
2015-09-09 20:05:24 +0530

    - Add functionality for previous button - Replace Next and Previous Buttons by ImageButtons

--- app/src/main/java/net/chi6rag/android/geoquiz/QuizActivity.java
+++ app/src/main/java/net/chi6rag/android/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,7 +15,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private TextView mQuestionTextView;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_africa, false),
             new Question(R.string.question_americas, true),
@@ -33,13 +35,21 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mQuestionTextView = (TextView) findViewById(R.id.question);
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
 
         // Set the text of mQuestionTextView to CurrentIndex of QuestionBank array
         int mQuestionResId = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(mQuestionResId);
 
         // Add event listeners to widgets
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion();
+            }
+        });
+
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,16 +74,38 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion(-1);
+            }
+        });
     }
 
     private void updateQuestion(){
+        // raise Error if difference >= mQuestionBank.length
         mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
-        int mQuestionResId = mQuestionBank[mCurrentIndex].getTextResId();
+        int mQuestionResId = mQuestionBank[ mCurrentIndex<0 ? -mCurrentIndex: mCurrentIndex ]
+                .getTextResId();
+        mQuestionTextView.setText(mQuestionResId);
+    }
+
+    private void updateQuestion(int difference){
+        // raise Error if difference >= mQuestionBank.length
+        // the values of difference can be:
+        //                                a. 0
+        //                                b. less than mQuestionBank.length
+        //                                c. more than mQuestionBank.length
+        mCurrentIndex = (mCurrentIndex+difference) % mQuestionBank.length;
+        int mQuestionResId = mQuestionBank[ mCurrentIndex<0 ? -mCurrentIndex: mCurrentIndex]
+                             .getTextResId();
         mQuestionTextView.setText(mQuestionResId);
     }
 
     private boolean checkAnswer(boolean userPressedAnswer){
-        boolean mQuestionAnswer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int index = mCurrentIndex<0 ? -mCurrentIndex: mCurrentIndex;
+        boolean mQuestionAnswer = mQuestionBank[index].isAnswerTrue();
         return mQuestionAnswer == userPressedAnswer;
     }
 
